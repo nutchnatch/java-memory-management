4ed79004f27fb011df99ce1cdd27176517bc12f6
nivaldomonteiro <nushnivaldo@gmail.com>
2021-03-04 00:50:09 +0000

    Added details about choosing jvm and improving application options.

--- PerformanceExample1/src/Main.java
+++ PerformanceExample1/src/Main.java
@@ -2,10 +2,16 @@ import java.util.Date;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+//		Thread.sleep(20000);
+		Date startDate = new Date();
+
 		PrimeNumbers primeNumbers = new PrimeNumbers();
 		Integer max = Integer.parseInt(args[0]);
 		primeNumbers.generateNumbers(max);
+
+		Date endDate = new Date();
+		System.out.println("Elapsed time was: " + (endDate.getTime() - startDate.getTime()) + " ms.");
 	}
 
 }
