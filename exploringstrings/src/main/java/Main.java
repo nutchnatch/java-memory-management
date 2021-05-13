a5c320e6dd5fdb6e78e70bf472e26ef1a9947f8a
nivaldomonteiro <nushnivaldo@gmail.com>
2021-05-13 23:50:45 +0100

    Added Tuning JVM memory - closes#3

---
+++ exploringstrings/src/main/java/Main.java
@@ -0,0 +1,20 @@
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Date start = new Date();
+        List<String> strings = new ArrayList<String>();
+        for(Integer i = 0; i < 10000000; i ++) {
+             //intern() will put the string into the stringpool
+            String s = i.toString().intern();
+            strings.add(s);
+        }
+
+        Date end = new Date();
+        System.out.println("Elapsed:" + (end.getTime() - start.getTime()) + " ms.");
+    }
+}
